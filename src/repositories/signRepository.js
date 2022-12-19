8cc97088d4dfd97475db7023d3e47d1eb9aacf4b
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-12-19 17:54:21 -0300

    shortenRepository.js & signRepository.js & visitRepository.js added

---
+++ src/repositories/signRepository.js
@@ -0,0 +1,30 @@
+import { connection } from '../../db/database.js';
+
+async function insertUser({ name, email, password }) {
+  const result = await connection.query(
+    `INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`,
+    [name, email, password]
+  );
+
+  return result;
+}
+
+async function getUserByEmail(email) {
+  return connection.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+}
+
+async function getTokenByUserId(userId) {
+  return connection.query(
+    `SELECT * FROM sessions WHERE "userId" = $1 AND active = TRUE;`,
+    [userId]
+  );
+}
+
+async function insertSessions({ userId, token }) {
+  return connection.query(
+    `INSERT INTO sessions ("userId", token) VALUES ($1, $2);`,
+    [userId, token]
+  );
+}
+
+export { insertUser, getUserByEmail, insertSessions, getTokenByUserId };
---
+++ src/repositories/visitRepository.js
@@ -0,0 +1,32 @@
+import { connection } from '../../db/database.js';
+
+async function upsertVisit(shortId) {
+  const result = await getVisit(shortId);
+  if (result.rowCount > 0) {
+    return incrementVisit(shortId);
+  }
+
+  return insertVisit(shortId);
+}
+
+async function insertVisit(shortId) {
+  return connection.query(
+    `INSERT INTO visits ("shortid", visit) VALUES ($1, $2);`,
+    [shortId, 1]
+  );
+}
+
+async function incrementVisit(shortId) {
+  return connection.query(
+    `UPDATE visits SET visit = visit + 1 WHERE "shortid" = $1;`,
+    [shortId]
+  );
+}
+
+async function getVisit(shortId) {
+  return connection.query(`SELECT * FROM visits WHERE "shortid" = $1;`, [
+    shortId,
+  ]);
+}
+
+export { getVisit, insertVisit, incrementVisit, upsertVisit };
