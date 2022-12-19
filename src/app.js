05175e656f46e940d2e9813c21166af85b8c7c7c
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-12-19 17:56:46 -0300

    app.js updated with routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connection } from '../db/database.js';
+import signRoute from './routes/signRoute.js';
+import urlRoute from './routes/urlRoute.js';
 
 dotenv.config();
 const server = express();
@@ -13,6 +15,9 @@ server.get('/status', async (req, res) => {
   const result = await connection.query('SELECT 1=1;');
   res.send(result.rows);
 });
+
+server.use(signRoute);
+server.use(urlRoute);
 
 server.listen(process.env.PORT, () => {
   console.log(`Listening to ${process.env.PORT}`);
