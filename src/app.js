5f99b8aa6c6bc5de798fc887b18af59c5e2876e5
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2023-05-23 17:05:18 -0300

    feat: configurations for Deploy

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ server.use(express.json());
 server.use(cors());
 server.use(routes);
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 server.listen(port, () =>
   console.log(`Server running in port: ${port}`)
